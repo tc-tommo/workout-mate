c1e10e18b5147d0a9a4f1c3e5170d1fc83d51c99
Tommo <tommo@chung.scot>
2025-03-20 12:21:12 +0000

    tld

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tommo.page',
+  site: 'https://workout.tommo.page',
   vite: {
     plugins: [tailwindcss()],
   },
